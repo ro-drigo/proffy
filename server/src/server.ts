896f4041a693f4d2a22df27a3a26098c6c1bd52e
ro-drigo <rodrigo.santos7202@gmail.com>
2020-08-04 13:00:23 -0300

    connection controller ok. add cors. backend ok.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 //fazer o express entender json
 app.use(express.json())
 
