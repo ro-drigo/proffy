2d59c2f3c87416968edc5171da0fa3de6ab1456c
ro-drigo <rodrigo.santos7202@gmail.com>
2020-08-05 13:28:00 -0300

    listclasses ok

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,27 +3,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/62777011?s=460&u=de4d0f8c49d1ffd8116f0b015f05c5a8131d8418&v=4" alt="Rodrigo Ferreira"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Rodrigo Ferreira</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Professor de química avançada.
-                <br/><br/>
-                Apaixonado por explodir laboratórios e fazer experiências malucas.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type="button">
                     <img src={whatsappIcon} alt="Whatsapp"/>
